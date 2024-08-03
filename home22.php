77b6ad123e8e32f4e67327c578a1d4fadcedf202
Software Engineering <husnain00241@gmail.com>
2024-08-03 05:07:06 -0700

    Add files via upload

---
+++ home22.php
@@ -0,0 +1,22 @@
+    <?php
+    $abc = "pakistan 266 445348/.,/.,;'";
+    $cd = 10;
+    $x = 20.5;
+
+    echo $abc;
+
+
+
+
+
+
+
+    ?>
+<html>
+    <head>
+        <title>home</title>
+    </head>
+    <body>
+        ikdfhgiheiu
+    </body>
+</html> 
